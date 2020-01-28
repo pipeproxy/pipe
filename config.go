392b22c8ba3954a5e5dda2db1dbf63c2c861d3cc
wzshiming <wzshiming@foxmail.com>
2020-01-28 23:23:37 +0800

    change components

--- config.go
+++ config.go
@@ -1,13 +1,12 @@
 package pipe
 
 import (
-	"github.com/wzshiming/pipe/components"
 	"github.com/wzshiming/pipe/once"
 	"github.com/wzshiming/pipe/service"
 )
 
 type Config struct {
+	Components []interface{}
 	Pipe       service.Service
-	Components components.Components
 	Init       []once.Once
 }
