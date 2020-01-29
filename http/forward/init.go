f0119a78e077e57119b95e35f089a6a3a426a13e
wzshiming <wzshiming@foxmail.com>
2020-01-29 22:04:26 +0800

    add http forward

--- http/forward/init.go
+++ http/forward/init.go
@@ -1,11 +1,15 @@
 package forward
 
 import (
+	"context"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"time"
 
 	"github.com/wzshiming/pipe/configure"
+	"github.com/wzshiming/pipe/stream"
 )
 
 const name = "forward"
@@ -15,7 +19,21 @@ func init() {
 }
 
 type Config struct {
-	Pass string
+	Forward stream.Handler
+	Pass    string
+}
+
+var defaultTransport = http.Transport{
+	DialContext: (&net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+		DualStack: true,
+	}).DialContext,
+	ForceAttemptHTTP2:     true,
+	MaxIdleConns:          100,
+	IdleConnTimeout:       90 * time.Second,
+	TLSHandshakeTimeout:   10 * time.Second,
+	ExpectContinueTimeout: 1 * time.Second,
 }
 
 // NewForwardWithConfig create a new forward with config.
@@ -24,5 +42,22 @@ func NewForwardWithConfig(conf *Config) (http.Handler, error) {
 	if err != nil {
 		return nil, err
 	}
-	return httputil.NewSingleHostReverseProxy(u), nil
+
+	rp := httputil.NewSingleHostReverseProxy(u)
+	rp.Transport = &defaultTransport
+	if conf.Forward != nil {
+		rp.Transport = &http.Transport{
+			DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+				p1, p2 := net.Pipe()
+				go conf.Forward.ServeStream(ctx, p1)
+				return p2, nil
+			},
+			ForceAttemptHTTP2:     defaultTransport.ForceAttemptHTTP2,
+			MaxIdleConns:          defaultTransport.MaxIdleConns,
+			IdleConnTimeout:       defaultTransport.IdleConnTimeout,
+			TLSHandshakeTimeout:   defaultTransport.TLSHandshakeTimeout,
+			ExpectContinueTimeout: defaultTransport.ExpectContinueTimeout,
+		}
+	}
+	return rp, nil
 }
