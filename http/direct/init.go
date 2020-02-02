6ded81d7309917c180538d7c88d739255c2d32f1
wzshiming <wzshiming@foxmail.com>
2020-02-02 16:41:33 +0800

    change http direct

--- http/direct/init.go
+++ http/direct/init.go
@@ -1,10 +1,12 @@
 package direct
 
 import (
+	"io/ioutil"
 	"net/http"
 
 	"github.com/wzshiming/pipe/configure"
 	"github.com/wzshiming/pipe/http/template"
+	"github.com/wzshiming/pipe/input"
 )
 
 const name = "direct"
@@ -15,11 +17,20 @@ func init() {
 
 type Config struct {
 	Code int
-	Body string
+	Body input.Input
 }
 
 func NewDirectWithConfig(conf *Config) (http.Handler, error) {
-	temp, err := template.NewFormat(conf.Body)
+	body, err := ioutil.ReadAll(conf.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = conf.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	temp, err := template.NewFormat(string(body))
 	if err != nil {
 		return nil, err
 	}
