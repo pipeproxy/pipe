98f26ea4137ae0b343e1d3d8cf0b8588d4227f4c
wzshiming <wzshiming@foxmail.com>
2020-02-22 00:16:49 +0800

    fix

--- internal/stream/network.go
+++ internal/stream/network.go
@@ -45,7 +45,7 @@ func Listen(ctx context.Context, network, address string) (listener.StreamListen
 		return n.Listener(ctx), nil
 	}
 
-	log.Printf("[INFO] ListenStream to %s", key)
+	log.Printf("[INFO] Listen to %s", key)
 	l, err := net.Listen(network, address)
 	if err != nil {
 		return nil, err
